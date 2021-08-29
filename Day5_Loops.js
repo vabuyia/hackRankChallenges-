b0539b6a487da6e8274b6be0bfafa5b854beee99
Abuya Obunga <vabuyia@gmail.com>
2021-08-29 15:00:57 +0300

    changes made

--- Day5_Loops.js
+++ Day5_Loops.js
@@ -1,14 +1,14 @@
+/****
+ * Day 5: Loops
 
+Task
+Given an integer, n, print its first 10 multiples. Each multiple n xi  (where ) 1<=1 i <11 should be 
+printed on a new line in the form: n x i = result.
 
-
-
-
-function printdis(n){
-    var i, result;
-    for (let i = 1; i <11; i++){
-        return result=n * i;
-        
-        }
-
-        
+*/
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    for (let i = 1; i <= 10; i += 1) {
+        console.log(`${n} x ${i} = ${n * i}`);
+    }
 }
